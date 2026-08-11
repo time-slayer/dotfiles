238d6842a0ac01ca5e12735aafe01163b406b1f8
TimeSlayer <68065991+time-slayer@users.noreply.github.com>
2026-08-11 12:01:54 +0200

    hyprland: add missing spaces in keybind string concatenation

--- .config/hypr/custom/lua/keybinds.lua
+++ .config/hypr/custom/lua/keybinds.lua
@@ -8,18 +8,18 @@ local mainMod = "SUPER"
 -- Programs
 ----------------------------------------------------
 local terminal = "kitty"
-hl.bind(mainMod .. "+ RETURN", hl.dsp.exec_cmd(terminal))
-hl.bind(mainMod .. "+ C", hl.dsp.exec_cmd("vscodium"))
-hl.bind(mainMod .. "+ T", hl.dsp.exec_cmd("Telegram"))
+hl.bind(mainMod .. " + RETURN", hl.dsp.exec_cmd(terminal))
+hl.bind(mainMod .. " + C", hl.dsp.exec_cmd("vscodium"))
+hl.bind(mainMod .. " + T", hl.dsp.exec_cmd("Telegram"))
 
 
 ----------------------------------------------------
 -- Window Management
 ----------------------------------------------------
-hl.bind(mainMod .. "+ Q", hl.dsp.window.close())
-hl.bind(mainMod .. "+ SHIFT + Q", hl.dsp.window.kill())
-hl.bind(mainMod .. "+ F", hl.dsp.window.fullscreen({ mode = "fullscreen" }))
-hl.bind(mainMod .. "+ SHIFT + F", hl.dsp.window.fullscreen({ mode = "maximized" }))
+hl.bind(mainMod .. " + Q", hl.dsp.window.close())
+hl.bind(mainMod .. " + SHIFT + Q", hl.dsp.window.kill())
+hl.bind(mainMod .. " + F", hl.dsp.window.fullscreen({ mode = "fullscreen" }))
+hl.bind(mainMod .. " + SHIFT + F", hl.dsp.window.fullscreen({ mode = "maximized" }))
 hl.bind("ALT + TAB", function()
   hl.dispatch(hl.dsp.window.cycle_next())
   hl.dispatch(hl.dsp.window.bring_to_top())
@@ -29,15 +29,15 @@ end)
 ----------------------------------------------------
 -- Layout
 ----------------------------------------------------
-hl.bind(mainMod .. "+ J", hl.dsp.layout("togglesplit"))
-hl.bind(mainMod .. "+ SHIFT + J", hl.dsp.layout("swapsplit"))
+hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))
+hl.bind(mainMod .. " + SHIFT + J", hl.dsp.layout("swapsplit"))
 
 
 ----------------------------------------------------
 -- Language Profiles
 ----------------------------------------------------
 local layout_profiles = require("custom.lua.layout_profiles")
-hl.bind(mainMod .. "+ SHIFT + K", function()
+hl.bind(mainMod .. " + SHIFT + K", function()
   layout_profiles.toggle()
 end)
 
@@ -45,4 +45,4 @@ end)
 ----------------------------------------------------
 -- Global
 ----------------------------------------------------
-hl.bind("SUPER + F10", hl.dsp.pass({ window = "class:^(com\\.obsproject\\.Studio)$" }))
+hl.bind(mainMod .. " + F10", hl.dsp.pass({ window = "class:^(com\\.obsproject\\.Studio)$" }))
