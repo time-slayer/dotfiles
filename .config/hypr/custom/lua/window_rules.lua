301c7853c6134b5f23c5d90dcc6e0be805ef0713
TimeSlayer <68065991+time-slayer@users.noreply.github.com>
2026-09-12 22:42:14 +0200

    hyprland: add focus rule for osu

--- .config/hypr/custom/lua/window_rules.lua
+++ .config/hypr/custom/lua/window_rules.lua
@@ -1,2 +1,3 @@
 hl.window_rule({ match = { class = "firefox" }, focus_on_activate = true })
 hl.window_rule({ match = { class = "codium" }, focus_on_activate = true })
+hl.window_rule({ match = { class = "osu!" }, focus_on_activate = true })
