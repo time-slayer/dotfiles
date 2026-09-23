31fd71116236c38210d3cc332a1fa066a1fd05a2
TimeSlayer <68065991+time-slayer@users.noreply.github.com>
2026-09-23 20:12:53 +0200

    hyprland: add zed to focus_on_activate window rule

--- .config/hypr/custom/lua/window_rules.lua
+++ .config/hypr/custom/lua/window_rules.lua
@@ -1,2 +1,2 @@
-local focus_on_active_classes = "^(firefox|codium|osu!)$"
+local focus_on_active_classes = "^(firefox|codium|dev\\.zed\\.Zed|osu!)$"
 hl.window_rule({ match = { class = focus_on_active_classes }, focus_on_activate = true })
